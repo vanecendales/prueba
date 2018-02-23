1789333515554ec16915a08b8cf87956e68bb8a6
Vanessa Cendales <vanecendales@gmail.com>
2018-02-23 09:50:38 -0500

    first release
    mobile tested

---
+++ js/common-scripts.js
@@ -0,0 +1,69 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
+var Script = function () {
+
+//    sidebar dropdown menu auto scrolling
+
+    jQuery('#sidebar .sub-menu > a').click(function () {
+        var o = ($(this).offset());
+        diff = 250 - o.top;
+        if(diff>0)
+            $("#sidebar").scrollTo("-="+Math.abs(diff),500);
+        else
+            $("#sidebar").scrollTo("+="+Math.abs(diff),500);
+    });
+
+//    sidebar toggle
+
+    $(function() {
+        function responsiveView() {
+            var wSize = $(window).width();
+            if (wSize <= 768) {
+                $('#container').addClass('sidebar-close');
+                $('#sidebar > ul').hide();
+            }
+
+            if (wSize > 768) {
+                $('#container').removeClass('sidebar-close');
+                $('#sidebar > ul').show();
+            }
+        }
+        $(window).on('load', responsiveView);
+        $(window).on('resize', responsiveView);
+    });
+
+    $('.i-menu').click(function () {
+        if ($('#sidebar > ul').is(":visible") === true) {
+            $('#main-content').css({
+                'margin-left': '0px'
+            });
+            $('#sidebar').css({
+                'margin-left': '-210px'
+            });
+            $('#sidebar > ul').hide();
+            $("#container").addClass("sidebar-closed");
+        } else {
+            $('#main-content').css({
+                'margin-left': '210px'
+            });
+            $('#sidebar > ul').show();
+            $('#sidebar').css({
+                'margin-left': '0'
+            });
+            $("#container").removeClass("sidebar-closed");
+        }
+    });
+
+//    tool tips
+
+    $('.tooltips').tooltip();
+
+//    popovers
+
+    $('.popovers').popover();
+
+
+
+
+}();
